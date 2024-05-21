972122e0164ba7c9330cbc4e2aa4b53923411098
yena <aneykrap@gmail.com>
2024-05-21 17:09:40 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+function Header(){
+  return <header>
+    <h1><a href="/" ></a></h1>
+  </header>
+}
+function App() {
+  return (
+    <div>
+      <Header Title = "WEB">
+      </Header>
+    </div>
+  );
+}
+
+export default App;
