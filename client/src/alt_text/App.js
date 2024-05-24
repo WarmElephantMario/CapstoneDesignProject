f3511ded1a20cb581628dab937976190628463ad
yena <aneykrap@gmail.com>
2024-05-24 22:14:37 +0900

    Update frontend

--- client/src/alt_text/App.js
+++ client/src/alt_text/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function Header(props) {
   return (
     <header>
-      <h1>{props.Title}</h1>
+      <h1>
+        <Link to={props.href}>{props.Title}</Link>
+      </h1>
     </header>
   );
 }
@@ -32,7 +35,7 @@ function App() {
 
   return (
     <div>
-      <Header Title="COMMA_DEMO" />
+      <Header Title="COMMA_DEMO" href ="/"/>
       <Article Title="이미지를 입력해주세요" />
       <div className="article-and-buttons">
         {upload}
