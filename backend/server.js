5e8f8847bf808d943d2b151a041522febe98ea14
SiwonChoi25 <chlznf@naver.com>
2024-06-01 00:24:14 +0900

    first commit

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ const fs = require('fs');
 const path = require('path');
 const OpenAIApi = require('openai'); // OpenAI 패키지 불러오기
 const os = require('os');
+const speech = require("@google-cloud/speech");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const http = require("http");
+const { Server } = require("socket.io");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
 
 // OpenAI API 설정
 const openai = new OpenAIApi('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');
@@ -16,6 +24,114 @@ app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true,
 }));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+//TODO: Create this file in the server directory of the project
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "./warmelephantmario.json";
+
+const speechClient = new speech.SpeechClient();
+
+
+io.on("connection", (socket) => {
+  let recognizeStream = null;
+  console.log("** a user connected - " + socket.id + " **\n");
+
+  socket.on("disconnect", () => {
+    console.log("** user disconnected ** \n");
+  });
+
+  socket.on("send_message", (message) => {
+    console.log("message: " + message);
+    setTimeout(() => {
+      io.emit("receive_message", "got this message" + message);
+    }, 1000);
+  });
+
+  socket.on("startGoogleCloudStream", function (data) {
+    startRecognitionStream(this, data);
+  });
+
+  socket.on("endGoogleCloudStream", function () {
+    console.log("** ending google cloud stream **\n");
+    stopRecognitionStream();
+  });
+
+  socket.on("send_audio_data", async (audioData) => {
+    io.emit("receive_message", "Got audio data");
+    if (recognizeStream !== null) {
+      try {
+        recognizeStream.write(audioData.audio);
+      } catch (err) {
+        console.log("Error calling google api " + err);
+      }
+    } else {
+      console.log("RecognizeStream is null");
+    }
+  });
+
+  function startRecognitionStream(client) {
+    console.log("* StartRecognitionStream\n");
+    try {
+      recognizeStream = speechClient
+        .streamingRecognize(request)
+        .on("error", console.error)
+        .on("data", (data) => {
+          const result = data.results[0];
+          const isFinal = result.isFinal;
+          const trans = data.results[0].alternatives[0].transcript;
+          // console.log(isFinal);
+
+          // const transcriptType = typeof trans;
+          // console.log("Type of transcript:" + transcriptType);
+          // console.log("자막:" + trans);
+
+          const transcription = data.results
+            .map((result) => result.alternatives[0].transcript)
+            .join("\n");
+
+          // console.log(`Transcription: `, transcription);
+
+          client.emit("receive_audio_text", {
+            text: transcription,
+            isFinal: isFinal,
+          });
+
+          // if end of utterance, let's restart stream
+          // this is a small hack to keep restarting the stream on the server and keep the connection with Google api
+          // Google api disconects the stream every five minutes
+          if (data.results[0] && data.results[0].isFinal) {
+            stopRecognitionStream();
+            startRecognitionStream(client);
+            console.log("restarted stream serverside");
+          }
+        });
+    } catch (err) {
+      console.error("Error streaming google api " + err);
+    }
+  }
+
+  function stopRecognitionStream() {
+    if (recognizeStream) {
+      console.log("* StopRecognitionStream \n");
+      recognizeStream.end();
+    }
+    recognizeStream = null;
+  }
+});
+
+
+
+
+
+//////////
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -88,6 +204,36 @@ app.post('/upload/subtitle', upload.single('file'), async (req, res) => {
 
 
 // 서버 시작
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 포트에서 실행 중입니다.`);
 });
+
+
+// =========================== GOOGLE CLOUD SETTINGS ================================ //
+
+// The encoding of the audio file, e.g. 'LINEAR16'
+// The sample rate of the audio file in hertz, e.g. 16000
+// The BCP-47 language code to use, e.g. 'en-US'
+const encoding = "LINEAR16";
+const sampleRateHertz = 16000;
+const languageCode = "ko-KR"; //en-US
+const alternativeLanguageCodes = ["en-US", "ko-KR"];
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: "ko-KR",
+    //alternativeLanguageCodes: alternativeLanguageCodes,
+    enableWordTimeOffsets: true,
+    enableAutomaticPunctuation: true,
+    enableWordConfidence: true,
+    enableSpeakerDiarization: true,
+    //diarizationSpeakerCount: 2,
+    //model: "video",
+    model: "command_and_search",
+    //model: "default",
+    useEnhanced: true,
+  },
+  interimResults: true,
+};
--- frontend/src/subtitle/Record.js
+++ frontend/src/subtitle/Record.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import './Record.css'
 import { Link } from 'react-router-dom';
+import { default as React, useEffect, useState, useRef } from "react";
+//import { Button } from "react-bootstrap";
+//import Container from "react-bootstrap/Container";
+import * as io from "socket.io-client";
 
 function Header(props) {
   return (
@@ -16,7 +20,28 @@ function Article(props) {
   return <article>{props.Title}</article>;
 }
 
-function Record() {
+const sampleRate = 16000;
+
+const getMediaStream = () =>
+  navigator.mediaDevices.getUserMedia({
+    audio: {
+      deviceId: "default",
+      sampleRate: sampleRate,
+      sampleSize: 16,
+      channelCount: 1,
+    },
+    video: false,
+  });
+
+const AudioToText = () => {
+  const [connection, setConnection] = useState();
+  const [currentRecognition, setCurrentRecognition] = useState();
+  const [recognitionHistory, setRecognitionHistory] = useState([]);
+  const [isRecording, setIsRecording] = useState(false);
+  const [recorder, setRecorder] = useState();
+  const processorRef = useRef();
+  const audioContextRef = useRef();
+  const audioInputRef = useRef();
   const [fileInfo, setFileInfo] = useState(null);
   const [file, setFile] = useState(null); // file 상태 추가
 
@@ -58,9 +83,111 @@ function Record() {
     }
   }
 
+  const speechRecognized = (data) => {
+    if (data.isFinal) {
+      setCurrentRecognition("...");
+      // setRecognitionHistory((old) => [data.text, ...old]);
+      // 최신 자막이 밑으로 쭉 출력되도록
+      setRecognitionHistory((old) => [...old, data.text]);
+    } else setCurrentRecognition(data.text + "...");
+  };
+
+  useEffect(() => {
+    console.log("\n\nrecognitionHistory", recognitionHistory);
+  }, [recognitionHistory]);
+
+  const connect = () => {
+    connection?.disconnect();
+    const socket = io.connect("http://localhost:5000");
+    socket.on("connect", () => {
+      console.log("connected", socket.id);
+      setConnection(socket);
+    });
+
+    socket.emit("send_message", "hello world");
+
+    socket.emit("startGoogleCloudStream");
+
+    socket.on("receive_message", (data) => {
+      console.log("received message", data);
+    });
+
+    socket.on("receive_audio_text", (data) => {
+      speechRecognized(data);
+      console.log("received audio text", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("disconnected", socket.id);
+    });
+  };
+
+  const disconnect = () => {
+    if (!connection) return;
+    connection?.emit("endGoogleCloudStream");
+    connection?.disconnect();
+    processorRef.current?.disconnect();
+    audioInputRef.current?.disconnect();
+    audioContextRef.current?.close();
+    setConnection(undefined);
+    setRecorder(undefined);
+    setIsRecording(false);
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (connection) {
+        if (isRecording) {
+          return;
+        }
+
+        const stream = await getMediaStream();
+
+        audioContextRef.current = new window.AudioContext();
+
+        await audioContextRef.current.audioWorklet.addModule(
+          "/src/worklets/recorderWorkletProcessor.js"
+        );
+
+        audioContextRef.current.resume();
+
+        audioInputRef.current =
+          audioContextRef.current.createMediaStreamSource(stream);
+
+        processorRef.current = new AudioWorkletNode(
+          audioContextRef.current,
+          "recorder.worklet"
+        );
+
+        processorRef.current.connect(audioContextRef.current.destination);
+        audioContextRef.current.resume();
+
+        audioInputRef.current.connect(processorRef.current);
+
+        processorRef.current.port.onmessage = (event) => {
+          const audioData = event.data;
+          connection.emit("send_audio_data", { audio: audioData });
+        };
+        setIsRecording(true);
+      } else {
+        console.error("No connection");
+      }
+    })();
+    return () => {
+      if (isRecording) {
+        processorRef.current?.disconnect();
+        audioInputRef.current?.disconnect();
+        if (audioContextRef.current?.state !== "closed") {
+          audioContextRef.current?.close();
+        }
+      }
+    };
+  }, [connection, isRecording, recorder]);
+
   const upload = <input type="button" value="업로드하기" className="button" onClick={handleUploadClick} />;
   const submit = <input type="button" value="submit" className="button" onClick={handleFileSubmit}/>;
-  const record = <input type="button" value="녹음시작" className="button" />;
+  const record = <input type="button" value="실시간자막시작" className="button" onClick={connect}/>;
+  const endRecord = <input type="button" value="실시간자막종료" className="button" onClick={disconnect}/>;
 
   return (
     <div>
@@ -70,6 +197,7 @@ function Record() {
         {upload}
         {submit}
         {record}
+        {endRecord}
       </div>
       <input
         type="file"
@@ -86,9 +214,14 @@ function Record() {
           </div>
         )}
       </div>
-      <div className="output-box"></div>
+      <div className="output-box">
+         {recognitionHistory.map((tx, idx) => (
+            <p key={idx}>{tx}</p>
+          ))}
+          <p>{currentRecognition}</p>
+      </div>
     </div>
   );
 }
 
-export default Record;
+export default AudioToText;
